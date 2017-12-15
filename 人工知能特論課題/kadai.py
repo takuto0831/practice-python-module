a6154bcf978fd76e94bc170f93d17694a03ded2f
takuto0831 <aseries2765@gmail.com>
2017-12-15 23:29:16 +0900

    first commit

--- 人工知能特論課題/kadai.py
+++ 人工知能特論課題/kadai.py
@@ -13,10 +13,40 @@ H = np.array([[1,-1,-1],
               [-1,2,3],
               [-1,3,5]])
 
+# 初期値
+start_point = np.array([[0],[0],[0]])
+
+# Armijo条件
+def armijo_method(x,d,H):
+    g,t = 0.1,0.5 # 初期条件
+    b = 1
+    while f(x+b*d,H) >= f(x,H) + g*b*(np.dot(df(x,H).T,d)):
+        b = b*t
+    return b
+
+# 最急降下法
+def gradient_descent_method(point,H):
+    eps  = 1e-5 # 終了条件
+    tmp = point # 点の移動用
+    history = point # 数値を記録
+    iteration = 1000 # 最大繰り返し回数
+
+    for i in range(iteration):
+        d = -df(tmp,H) # 探索方向
+        alpha = armijo_method(tmp,d,H) # ステップ幅
+        tmp_new = tmp + alpha*d
+        if np.abs(np.linalg.norm(tmp-tmp_new)) < eps:
+            break
+        tmp = tmp_new
+        history = np.concatenate([history,tmp],axis = 1)
+    return history.T
+
 # 関数
 def f(x,H):
-    x
-    return 4*x[0]**2 + 2*x[0]*x[1] + 2*x[1]**2 - 2*x[0] - 4*x[1]
+    return x.sum() - 0.5*np.dot(x.T,np.dot(H,x))
 
 # 導関数
-def df(x):
+def df(x,H):
+    return np.array([[1],[1],[1]]) - np.dot(H,x)
+
+data_gradient = gradient_descent_method(start_point,H)
